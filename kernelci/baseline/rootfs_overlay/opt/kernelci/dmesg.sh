7b1f3703d23aa7dbc0693cd08d384c4726d91fe3
Paweł Wieczorek <pawiecz@collabora.com>
2025-01-20 22:27:33 +0100

    wip: Align dmesg.sh script with Debian-based rootfs
    Signed-off-by: Paweł Wieczorek <pawiecz@collabora.com>

--- kernelci/baseline/rootfs_overlay/opt/kernelci/dmesg.sh
+++ kernelci/baseline/rootfs_overlay/opt/kernelci/dmesg.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+ret=0
+
 if [ "$KERNELCI_LAVA" = "y" ]; then
     alias test-result='lava-test-case'
 else
@@ -10,7 +12,17 @@ fi
 
 for level in crit alert emerg; do
     dmesg --level=$level --notime -x -k > dmesg.$level
-    test -s dmesg.$level && res=fail || res=pass
+    if grep -q "No irq handler for vector" dmesg.$level; then
+        echo "Ignoring:"
+        grep "No irq handler for vector" dmesg.$level
+        sed '/No irq handler for vector/d' dmesg.$level > dmesg.$level
+    fi
+    if [ -s "dmesg.$level" ]; then
+        res=fail
+        ret=1
+    else
+        res=pass
+    fi
     count=$(cat dmesg.$level | wc -l)
     cat dmesg.$level
     test-result \
@@ -20,4 +32,4 @@ for level in crit alert emerg; do
         --units lines
 done
 
-exit 0
+exit $ret
